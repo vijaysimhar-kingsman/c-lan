c61d103b7c8634e2cf3acc27c6a1bb4d85834624
vijaysimhar-kingsman <vijayasimharockz@gmail.com>
2021-04-15 20:55:08 +0530

    this program will help you find unique characters in the given input string

--- unq_char.c
+++ unq_char.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-	char str1[]="vijaysimhareddy";
+	char str1[20];
+	printf("enter a string: ");
+	scanf("%s",str1);
 //finding the length of the string
 	int len=0;
 	while(str1[len]!='\0'){	
@@ -11,21 +13,29 @@ int main(){
 	char str2[len];
 	
 	int i=0;
-//assigning all cahracters of the second array to  null characters to increse the speeb # in loops
+	int arr[len];
+/*assigning all cahracters of the second array to 
+ null characters to increse the speeb # in loops
+ and assigning array values to 0 */ 
 	while(i<len){
 		str2[i]='\0';
-		printf("%c ",str2[i]);
+		arr[i]=0;
+	
 		i++;
 	}
 	i=0;
+	
 //looping our data
 	while(i<len){
 		int j=0;
 		while(str2[j]!='\0'){
 			if(str2[j]!=str1[i]){
 				j++;
-			}else{
-				str2[j]='*';//if data matches we are making that data to *
+			}else if(str2[j]==str1[i]){
+				/*increasing array values will help us in the end
+				 to know the unique values and also
+				 , to know how many times a character repeated */
+				arr[j]++;
 				break;
 			}
 		}if(str2[j]=='\0'){
@@ -35,12 +45,12 @@ int main(){
 	int j=0;
 	printf("\n");
 	while(str2[j]!='\0'){
-		if(str2[j]!='*'){
-			// we are not printing the astric values our second array will look like v*j**smhre*
+		if(arr[j]==0){
+			//if the array[i] value is 0 character respective to the index in the str2 is unique
 			printf(" %c ",str2[j]);
 			
 		}j++;
-		//and our output will be for this data is : v j s m h r e //
+		
 	}printf("\n these are the unique characters from the string : %s",str1);
 	
 }//vijayasimharockz@gmail.com
